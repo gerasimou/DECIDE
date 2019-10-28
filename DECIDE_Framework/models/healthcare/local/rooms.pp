f0fc4d659e2e01d1ec8b07c900b93f9c8ebacfe1
Javier Camara Moreno <javier.camaramoreno@york.ac.uk>
2019-10-28 14:33:11 +0000

    - Simplified local room model template to use same constant values for different rooms

--- DECIDE_Framework/models/healthcare/local/rooms.pp
+++ DECIDE_Framework/models/healthcare/local/rooms.pp
@@ -8,43 +8,32 @@ ctmc
 
 // Model parameters
 
-const double t; // Time threshold constant for time-related PCTL property
 const mf =100; // Used for calculation of state values in rooms
 const GO=0; // Initial state
-const travel_time;
 
 // Are we there yet?
-#for i=NRT1+NRT2:NRT1+NRT2#
-label "end" = s=r#i#END;
-#end#
+label "end" = s=r#NRT1+NRT2#END;
 
 // Rooms Type 1 -----------
 
-#for i=1:NRT1#
 // Begin Room Constants ---
-const double r#i#l1i; const double r#i#l2i; const double r#i#l2ip;
-const double r#i#p2norm; const double r#i#p2retry;
-const double r#i#c1i; const double r#i#c2i;
 
+#for i=1:NRT1#
 const r#i#T1=#i#*mf+0; const r#i#T2=#i#*mf+1; const r#i#T2P=#i#*mf+2; const r#i#END=#i#*mf+3;
+#end#
+
 // End Room Constants ----
 
-#end#
 
 // Rooms Type 2 -----------
 
-#for i=NRT1+1:NRT1+NRT2#
 // Begin Room Constants ---
 
-const double r#i#l1i; const double r#i#l2i; const double r#i#l3i; const double r#i#l3ip;
-const double r#i#p2req; const double r#i#p2retry; 
-const double r#i#p3poss; const double r#i#p3ibasic; const double r#i#p3ifull; 
-const double r#i#c1i; const double r#i#c2i; const double r#i#c3i; const double r#i#c3ip;
-const double r#i#u3; const double r#i#u3p;
-
+#for i=NRT1+1:NRT1+NRT2#
 const r#i#T1=#i#*mf+0; const r#i#T2=#i#*mf+1; const r#i#T3=#i#*mf+2; const r#i#T3P=#i#*mf+3; const r#i#END=#i#*mf+4;
-// End Room Constants ----
 #end#
+
+// End Room Constants ----
 
 module rooms
   s:[GO..r#NRT1+NRT2#END] init GO;
@@ -55,43 +44,43 @@ module rooms
 // Rooms Type 1
 
 #for i=1:NRT1#
-  [r#i#T1done] (s=r#i#T1) -> r#i#l1i*r#i#p2norm: (s'=r#i#T2) + r#i#l1i*(1-r#i#p2norm): (s'=r#i#T2P);
-  [r#i#T2done] (s=r#i#T2) -> r#i#l2i*r#i#p2retry: (s'=r#i#T2) + r#i#l2i*(1-r#i#p2retry): (s'=r#i+1#T1);
-  [r#i#T2done] (s=r#i#T2P) -> r#i#l2ip*r#i#p2retry: (s'=r#i#T2P) + r#i#l2ip*(1-r#i#p2retry): (s'=r#i+1#T1);
+  [r#i#T1done] (s=r#i#T1) -> rt1l1i*rt1p2norm: (s'=r#i#T2) + rt1l1i*(1-rt1p2norm): (s'=r#i#T2P);
+  [r#i#T2done] (s=r#i#T2) -> rt1l2i*rt1p2retry: (s'=r#i#T2) + rt1l2i*(1-rt1p2retry): (s'=r#i+1#T1);
+  [r#i#T2done] (s=r#i#T2P) -> rt1l2ip*rt1p2retry: (s'=r#i#T2P) + rt1l2ip*(1-rt1p2retry): (s'=r#i+1#T1);
 #end#
 
 // Rooms Type 2
 
 #for i=NRT1+1:NRT1+NRT2-1#
-[r#i#T1done] (s=r#i#T1) -> r#i#l1i*(1-r#i#p2req)*(1-r#i#p3poss): (s'=r#i+1#T1) +
-                     r#i#l1i*(1-r#i#p2req)*r#i#p3poss*r#i#p3ifull: (s'=r#i#T3) +
-                     r#i#l1i*r#i#p2req: (s'=r#i#T2) +
-                     r#i#l1i*(1-r#i#p2req)*r#i#p3poss*r#i#p3ibasic: (s'=r#i#T3P);
+[r#i#T1done] (s=r#i#T1) -> rt2l1i*(1-rt2p2req)*(1-rt2p3poss): (s'=r#i+1#T1) +
+                     rt2l1i*(1-rt2p2req)*rt2p3poss*rt2p3ifull: (s'=r#i#T3) +
+                     rt2l1i*rt2p2req: (s'=r#i#T2) +
+                     rt2l1i*(1-rt2p2req)*rt2p3poss*rt2p3ibasic: (s'=r#i#T3P);
 
-[r#i#T2done] (s=r#i#T2) -> r#i#l2i*(1-r#i#p2retry)*r#i#p3poss*r#i#p3ifull: (s'=r#i#T3) +
-                     r#i#l2i*(1-r#i#p2retry)*(1-r#i#p3poss): (s'=r#i+1#T1) +
-                     r#i#l2i*(1-r#i#p2retry)*r#i#p3poss*(1-r#i#p3ifull): (s'=r#i#T3P) +
-                     r#i#l2i*r#i#p2retry: (s'=r#i#T2);  
+[r#i#T2done] (s=r#i#T2) -> rt2l2i*(1-rt2p2retry)*rt2p3poss*rt2p3ifull: (s'=r#i#T3) +
+                     rt2l2i*(1-rt2p2retry)*(1-rt2p3poss): (s'=r#i+1#T1) +
+                     rt2l2i*(1-rt2p2retry)*rt2p3poss*(1-rt2p3ifull): (s'=r#i#T3P) +
+                     rt2l2i*rt2p2retry: (s'=r#i#T2);  
 
-[r#i#T3done] (s=r#i#T3) -> r#i#l3i: (s'=r#i+1#T1);
+[r#i#T3done] (s=r#i#T3) -> rt2l3i: (s'=r#i+1#T1);
 
-[r#i#T3Pdone] (s=r#i#T3P) -> r#i#l3ip: (s'=r#i+1#T1);
+[r#i#T3Pdone] (s=r#i#T3P) -> rt2l3ip: (s'=r#i+1#T1);
 #end#
 
 #for i=NRT1+NRT2:NRT1+NRT2#
-[r#i#T1done] (s=r#i#T1) -> r#i#l1i*(1-r#i#p2req)*(1-r#i#p3poss): (s'=r#i#END) +
-                     r#i#l1i*(1-r#i#p2req)*r#i#p3poss*r#i#p3ifull: (s'=r#i#T3) +
-                     r#i#l1i*r#i#p2req: (s'=r#i#T2) +
-                     r#i#l1i*(1-r#i#p2req)*r#i#p3poss*r#i#p3ibasic: (s'=r#i#T3P);
+[r#i#T1done] (s=r#i#T1) -> rt2l1i*(1-rt2p2req)*(1-rt2p3poss): (s'=r#i#END) +
+                     rt2l1i*(1-rt2p2req)*rt2p3poss*rt2p3ifull: (s'=r#i#T3) +
+                     rt2l1i*rt2p2req: (s'=r#i#T2) +
+                     rt2l1i*(1-rt2p2req)*rt2p3poss*rt2p3ibasic: (s'=r#i#T3P);
 
-[r#i#T2done] (s=r#i#T2) -> r#i#l2i*(1-r#i#p2retry)*r#i#p3poss*r#i#p3ifull: (s'=r#i#T3) +
-                     r#i#l2i*(1-r#i#p2retry)*(1-r#i#p3poss): (s'=r#i#END) +
-                     r#i#l2i*(1-r#i#p2retry)*r#i#p3poss*(1-r#i#p3ifull): (s'=r#i#T3P) +
-                     r#i#l2i*r#i#p2retry: (s'=r#i#T2);  
+[r#i#T2done] (s=r#i#T2) -> rt2l2i*(1-rt2p2retry)*rt2p3poss*rt2p3ifull: (s'=r#i#T3) +
+                     rt2l2i*(1-rt2p2retry)*(1-rt2p3poss): (s'=r#i#END) +
+                     rt2l2i*(1-rt2p2retry)*rt2p3poss*(1-rt2p3ifull): (s'=r#i#T3P) +
+                     rt2l2i*rt2p2retry: (s'=r#i#T2);  
 
-[r#i#T3done] (s=r#i#T3) -> r#i#l3i: (s'=r#i#END);
+[r#i#T3done] (s=r#i#T3) -> rt2l3i: (s'=r#i#END);
 
-[r#i#T3Pdone] (s=r#i#T3P) -> r#i#l3ip: (s'=r#i#END);
+[r#i#T3Pdone] (s=r#i#T3P) -> rt2l3ip: (s'=r#i#END);
 #end#
 
 
@@ -101,14 +90,14 @@ endmodule
 // Cost reward
 rewards "c"
 #for i=1:NRT1#
-  [r#i#T1done] true: r#i#c1i;
-  [r#i#T2done] true: r#i#c2i;
+  [r#i#T1done] true: rt1c1i;
+  [r#i#T2done] true: rt1c2i;
  #end#
 #for i=NRT1+1:NRT1+NRT2#
-  [r#i#T1done] true: r#i#c1i;
-  [r#i#T2done] true: r#i#c2i;
-  [r#i#T3done] true: r#i#c3i;
-  [r#i#T3Pdone] true: r#i#c3ip;
+  [r#i#T1done] true: rt2c1i;
+  [r#i#T2done] true: rt2c2i;
+  [r#i#T3done] true: rt2c3i;
+  [r#i#T3Pdone] true: rt2c3ip;
 #end#
 
 endrewards
@@ -116,8 +105,8 @@ endrewards
 // Utility reward
 rewards "u"
  #for i=NRT1+1:NRT1+NRT2#
-  [r#i#T3done] true: r#i#u3;
-  [r#i#T3Pdone] true: r#i#u3p;
+  [r#i#T3done] true: rt2u3;
+  [r#i#T3Pdone] true: rt2u3p;
  #end#
 endrewards
 
@@ -125,3 +114,21 @@ endrewards
 rewards "t"
    true : 1;
 endrewards
+
+
+// Constants to append
+
+//const double t;
+//const double travel_time;
+
+// Rooms Type 1 -----------
+// const double rt1l1i; const double rt1l2i; const double rt1l2ip;
+// const double rt1p2norm; const double rt1p2retry;
+// const double rt1c1i; const double rt1c2i;
+
+// Rooms Type 2 -----------
+// const double rt2l1i; const double rt2l2i; const double rt2l3i; const double rt2l3ip;
+// const double rt2p2req; const double rt2p2retry; 
+// const double rt2p3poss; const double rt2p3ibasic; const double rt2p3ifull; 
+// const double rt2c1i; const double rt2c2i; const double rt2c3i; const double rt2c3ip;
+// const double rt2u3; const double rt2u3p;
