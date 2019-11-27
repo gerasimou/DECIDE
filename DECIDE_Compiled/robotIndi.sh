6ffa4dc99e7fc06b8dc37125beccb886e4f69555
Simos Gerasimou <simos@cs.york.ac.uk>
2019-11-27 00:22:07 +0000

    cleaned up the repo

---
+++ DECIDE_Compiled/robotIndi.sh
@@ -0,0 +1,135 @@
+#!/bin/bash 
+
+#-------------------------------------------------------
+#  Variables
+#-------------------------------------------------------
+TARGET_DIR=`pwd`/target
+ROBOT_ID=$1
+FAT_JAR="${TARGET_DIR}/DECIDE-jar-with-dependencies.jar"
+DECIDE_JAR=DECIDEfat
+
+RUN="yes"
+
+
+
+#-------------------------------------------------------
+#  Function for killing processes
+#  Params: (1) Tab name, (2): command to execute
+#-------------------------------------------------------
+function killAll() {
+	processes=(${DECIDE_JAR})
+	for i in ${processes[@]}; do
+		printf "%s\t\n" ${i}
+		kill -9 $(ps aux | grep ${i} | awk '{print $2}')
+	done
+}
+
+
+function initAndRun() {
+	
+	# remove target/UUV dir
+	if [ -d ${TARGET_DIR}/ROBOT${ROBOT_ID} ]; then
+		rm -rf ${TARGET_DIR}/ROBOT${ROBOT_ID}
+	fi
+
+	
+
+#TARGET_DIR=`pwd`/target
+
+#FAT_JAR="${TARGET_DIR}/DECIDE-jar-with-dependencies.jar"
+#DECIDE_JAR=DECIDEfat
+	#-------------------------------------------------------
+	#  Rename fat jar
+	#-------------------------------------------------------
+	if [ -f ${FAT_JAR} ]; then
+		printf "\n\nRenaming DECIDE fat jar\n"
+		mv ${FAT_JAR} ${TARGET_DIR}/${DECIDE_JAR}.jar
+		chmod 700 ${TARGET_DIR}/${DECIDE_JAR}.jar
+	fi
+
+	#-------------------------------------------------------
+	#  Create a single component system
+	#	added log4j compatibility line58
+	#-------------------------------------------------------
+	printf "\n\nCreating COMP%s directory for Robot #%s\n" ${ROBOT_ID} ${ROBOT_ID}
+	COMP_DIR=${TARGET_DIR}/ROBOT${ROBOT_ID}
+	mkdir ${COMP_DIR}
+	cp ${TARGET_DIR}/${DECIDE_JAR}.jar 	${COMP_DIR}/
+	mkdir -p ${COMP_DIR}/resources/healthcare
+	cp -r ${TARGET_DIR}/resources/healthcare/config${ROBOT_ID}.properties	${COMP_DIR}/resources/healthcare/
+	mv ${COMP_DIR}/resources/healthcare/config${ROBOT_ID}.properties			${COMP_DIR}/resources/healthcare/config.properties	
+	#cp ${TARGET_DIR}/resources/log4j2.xml		${COMP_DIR}/resources/
+	#cp ${TARGET_DIR}/resources/logging.log		${COMP_DIR}/resources/
+	mkdir ${COMP_DIR}/models 
+	cp -r ${TARGET_DIR}/models/healthcare						${COMP_DIR}/models/
+	cp -r ${TARGET_DIR}/repo 													${COMP_DIR}/
+
+	#-------------------------------------------------------
+	#  Execute, please remove comment
+	#-------------------------------------------------------
+	cd ${COMP_DIR}; 
+
+	if [[ "$OSTYPE" == "linux-gnu" ]]; then
+		export LD_LIBRARY_PATH=repo/prism-4.5:$LD_LIBRARY_PATH
+		echo $LD_LIBRARY_PATH
+		PRISM_JAVA="$JAVA_HOME"/bin/java
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		export DYLD_LIBRARY_PATH=repo/prism-4.5:$DYLD_LIBRARY_PATH
+		echo $DYLD_LIBRARY_PATH
+		PRISM_JAVA=`/usr/libexec/java_home`"/bin/java" 
+	else 
+		printf "%s prism files not implemented yet..." $OSTYPE
+		exit 0
+	fi
+
+
+	#Run
+	echo $PRISM_JAVA	
+	$PRISM_JAVA -jar ${DECIDE_JAR}.jar
+
+
+	#PRISM_JAVA=`/usr/libexec/java_home`"/bin/java" 
+	#export DYLD_LIBRARY_PATH=repo/prism:$DYLD_LIBRARY_PATH; 
+	#echo $DYLD_LIBRARY_PATH
+	#$PRISM_JAVA -jar ${DECIDE_JAR}.jar
+	#createNewTab "COMP1" "cd ${TARGET_DIR}/COMP1; export DYLD_LIBRARY_PATH=repo/prism:$DYLD_LIBRARY_PATH; java -jar ${DECIDE_JAR}.jar &"
+	#sleep 60
+	#createNewTab "COMP2" "cd ${TARGET_DIR}/COMP2; export DYLD_LIBRARY_PATH=repo/prism:$DYLD_LIBRARY_PATH; java -jar ${DECIDE_JAR}.jar &"
+}
+
+
+
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+	printf "%s [SWITCHES]                       \n" $0
+	printf "  --verbose                         \n" 
+	printf "  --help, -h                        \n" 
+	exit 0;	
+	elif [ "${ARGI}" = "kill" -o "${ARGI}" = "-k" ] ; then
+		RUN="no"
+#	elif [ "${ARGI}" = "remove" -o "${ARGI}" = "-r" ] ; then
+#		REMOVE="yes"
+#   elif [ "${ARGI}" = "--verbose" -o "${ARGI}" = "-v" ] ; then
+#	VERBOSE="-v"
+#    else 
+#	printf "Bad Argument: %s \n" $ARGI
+#	exit 0
+    fi
+done
+
+
+
+
+#-------------------------------------------------------
+#  Part 2: Act accordingly
+#-------------------------------------------------------
+if [ ${RUN} = "yes" ]; then
+	printf "Running\n"
+	initAndRun
+else
+	printf "Killing all\n"
+    killAll
+fi
---
+++ DECIDE_Compiled/target/ROBOT1/models/healthcare/global/gallocsp.pp
@@ -0,0 +1,87 @@
+
+// Healthcare case study global allocation model template
+// ------------------------------------------------------
+// This MDP model takes as input the number of robots, capabilities per robot, 
+// and room number of every type, and can be used to
+// solve the optimization problem via MDP policy synthesis
+// Author: Javier Camara / 2019
+//---------------------------------------------------------
+
+mdp
+
+#const NROBOTS# // NROBOTS: Number of robots
+#const CAPABILITIES# // CAPABILITIES: Number of capabilities/robot (currently fixed)
+#const NROOMTYPES# // Number of room types
+
+//#const NROOMST1=4;
+//#const NROOMST2=4;
+
+const Max_cost=300;
+const Max_time=300;
+
+const double Wr = 1;
+const double Zr = 1;
+
+formula done = #for k=1:NROOMTYPES# (allocatedt#k#=0) & #end# true;
+
+formula NROOMS = #for k=1:NROOMTYPES# NROOMST#k# + #end# 0;
+
+//-----------------------------------
+// Rooms 
+//-----------------------------------
+
+module rooms
+ time: [0..Max_time] init 0;
+ cost: [0..Max_cost] init 0;
+ end: bool init false;
+
+ #for i=1:NROOMTYPES#
+  allocatedt#i#: [0..NROOMST#i#] init NROOMST#i#;
+ #end#
+
+#for k=1:NROOMTYPES#
+  #for i=1:NROBOTS#
+   #for j=1:CAPABILITIES#
+  [r#i#c#j#t#k#] (allocatedt#k#>0) & (cost+costr#i#c#j#t#k#<=Max_cost) & (time+timer#i#c#j#t#k#<=Max_time)  -> (allocatedt#k#'=allocatedt#k#-1) & (cost'=cost+costr#i#c#j#t#k#) & (time'=time+timer#i#c#j#t#k#);
+   #end#
+  #end#
+#end#
+  [] done -> (end'=true);
+endmodule
+
+//-----------------------------------
+// Robots 
+//-----------------------------------
+
+#for i=1:NROBOTS#
+
+module r#i#
+ r#i#counter: [0..NROOMS] init 0;
+ #for j=1:CAPABILITIES#
+   #for k=1:NROOMTYPES#
+ [r#i#c#j#t#k#] (r#i#counter<NROOMS) -> (r#i#counter'=r#i#counter+1);
+   #end#
+ #end#
+endmodule
+#end#
+
+//---------------------------------------
+// Global utility
+//---------------------------------------
+
+#for i=1:NROBOTS#
+ formula diffs#i# = #for j=1:NROBOTS# (max(r#i#counter, r#j#counter) - min(r#i#counter, r#j#counter)) + #end# 0;
+#end#
+
+formula diffs = (NROOMS * NROOMS) - (#for i=1:NROBOTS# diffs#i# + #end# 0);
+
+rewards "utility"
+#for i=1:NROBOTS#
+ #for j=1:CAPABILITIES#
+    #for k=1:NROOMTYPES#
+  [r#i#c#j#t#k#] true: Wr;
+   #end#
+  #end#
+  done : Zr * diffs;
+#end#
+endrewards
